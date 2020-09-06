ff1f1473718abbba224d8c339d637c27450b9a6e
Jakub Igla <jakub.igla@zuhlke.com>
2020-09-06 19:36:18 +0100

    ADd proper installer

--- tools/install.sh
+++ tools/install.sh
@@ -1,32 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-xcode-select --install || true
+BRANCH=${1:-master}
 
-if ! command -v ansible-playbook &> /dev/null; then
-  sudo -H pip3 install ansible
-fi
+git clone git@github.com:jakubigla/mac-dev-playbook.git ~/mac-dev-playbook
+cd ~/mac-dev-playbook
 
-ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -i inventory -K
+git checkout $BRANCH
 
-apm install \
-  go-plus \
-  go-debug \
-  go-signature-statusbar \
-  file-icons \
-  highlight-selected \
-  minimap \
-  minimap-highlight-selected \
-  monokai \
-  pp-markdown \
-  language-terraform \
-  local-history
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-  git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-fi
+./tools/run.sh
